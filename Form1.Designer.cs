910385ea4d31c77f6fbc380689b0ec1aaee265f3
Moritz Preining <preiningmoritz@gmail.com>
2022-04-27 16:33:20 +0200

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace LaunchApps
+{
+    partial class AppLauncher
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(260, 138);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(250, 150);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // AppLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Start);
+            this.Name = "AppLauncher";
+            this.Text = "AppLauncher";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Start;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace LaunchApps
+{
+    public partial class AppLauncher : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public AppLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process flash = new Process();
+            flash.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+
+            flash.StartInfo.FileName = "C:\\Windows\\System32\\cmd.exe";
+            flash.Start();
+            Thread.Sleep(200);
+
+            ShowWindow(flash.MainWindowHandle, 3);
+            MoveWindow(flash.MainWindowHandle, Screen.AllScreens[0].Bounds.X, Screen.AllScreens[0].Bounds.Y, Screen.AllScreens[0].Bounds.Width, Screen.AllScreens[0].Bounds.Height, true);
+        }
+
+    }
+}
